aebc0aab607d33b8f3f260572b4eb71d170a6fe8
Drainyyyy <Drainyyyy@users.noreply.github.com>
2019-03-05 21:25:05 +0100

    Added interface for the file creators

---
+++ src/com/github/drainyyyy/manageStream/file/File.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018 - 2019. Drainyyy
+ * This project is covered by MIT License
+ * https://opensource.org/licenses/MIT
+ */
+
+package com.github.drainyyyy.manageStream.file;
+
+public interface File {
+
+    void start();
+    void delete();
+
+}
